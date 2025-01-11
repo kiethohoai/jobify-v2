4a8f34f596b88b5c61f7358a6e4b1ae47defa787
David Ho <kietho.060193>
2025-01-11 16:55:42 +0700

    04 Landing Page

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/images/logo.svg';
+
+const Logo = () => {
+  return (
+    <nav>
+      <img src={logo} alt="logo" />
+    </nav>
+  );
+};
+export default Logo;
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,6 +1,37 @@
+import Wrapper from '../assets/wrappers/LandingPage';
+import main from '../assets/images/main.svg';
+import { Link } from 'react-router-dom';
+import { Logo } from '../components';
+
 const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
-export default Landing
+    <Wrapper>
+      <Logo />
+
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>job tracking app</h1>
+          <p>
+            {`I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
+            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
+            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
+            narwhal.`}
+          </p>
+
+          {/* btn */}
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+
+        {/* image */}
+        <img className="img main-img" src={main} alt="main image" />
+      </div>
+    </Wrapper>
+  );
+};
+export default Landing;
