4f4eb87ea8dc64bdb8a44ae0f70941e14e2fe94e
David Ho <kietho.060193>
2025-01-15 17:23:00 +0700

    Hash Password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,25 @@
 import User from '../models/UserModel.js';
 import { StatusCodes } from 'http-status-codes';
+import { hashPassword } from '../utils/passwordUtils.js';
 
 export const register = async (req, res) => {
+  // auto add role for user
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? 'admin' : 'user';
 
+  // hash password
+  // const salt = await bcrypt.genSalt(10);
+  // const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  // req.body.password = hashedPassword;
+
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
+
+  // create user
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+
+  // send back result
+  res.status(StatusCodes.CREATED).json({ msg: 'User Created' });
 };
 
 export const login = async (req, res) => {
